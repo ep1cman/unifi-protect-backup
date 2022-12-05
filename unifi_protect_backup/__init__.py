34bc37bd0b7d346feebc1f3f0ee7b2a7ec66643a
Sebastian Goscik <sebastian.goscik@live.co.uk>
2022-12-05 14:27:11 +0000

    Bump version: 0.8.1 → 0.8.2

--- unifi_protect_backup/__init__.py
+++ unifi_protect_backup/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """sebastian.goscik"""
 __email__ = 'sebastian@goscik.com'
-__version__ = '0.8.1'
+__version__ = '0.8.2'
 
 # from .unifi_protect_backup import UnifiProtectBackup
 from .downloader import VideoDownloader
