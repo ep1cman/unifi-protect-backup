d9af6a03a529dd5763cbd2f9c15da9b94dd28e28
Sebastian Goscik <sebastian.goscik@live.co.uk>
2023-03-08 00:35:37 +0000

    fix isort induced circular import

--- unifi_protect_backup/__init__.py
+++ unifi_protect_backup/__init__.py
@@ -4,9 +4,9 @@ __author__ = """sebastian.goscik"""
 __email__ = 'sebastian@goscik.com'
 __version__ = '0.8.8'
 
-# from .unifi_protect_backup_core import UnifiProtectBackup
 from .downloader import VideoDownloader
 from .event_listener import EventListener
-from .missing_event_checker import MissingEventChecker
 from .purge import Purge
 from .uploader import VideoUploader
+
+from .missing_event_checker import MissingEventChecker  # isort: skip
